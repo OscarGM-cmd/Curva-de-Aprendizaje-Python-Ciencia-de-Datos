13e87789876d4ed3f31bf8f37db6bd5ec01feb6e
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-08-27 01:48:39 -0600

    Se agregaron comentarios de como funciona ese bloque de codigo

--- Output_Operacion_Aritmetica.py
+++ Output_Operacion_Aritmetica.py
@@ -1,3 +1,7 @@
+#En este bloque de codigo lo que hace es crear una lista con n espacios, luego un ciclo for de 0 a 4 basicamente 4 ciclos, posteriormente
+#se le agrego un mensaje en pantalla para depues guardar el valor en x, con append se le guarda el valor al ultimo indice de la lista 
+#como esta vacia el ultimo indice de esa lista es -1, despues es 0,1,2,3, la funcion aritmetica era:
+#(a+b/c*d)^2
 lista = []
 for i in range(0, 4):
     print(f'Ingresa el valor de el modulo {i}: ')
