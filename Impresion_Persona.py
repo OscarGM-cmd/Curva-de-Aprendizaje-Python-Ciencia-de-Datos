a9e09fd06e8048e44dba0cece879fcfacbdeb817
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-08-27 01:06:55 -0600

    feat: Implementación de I/O para captura y saludo de usuario

---
+++ Impresion_Persona.py
@@ -0,0 +1,6 @@
+"""
+La funcion de este bloque de codigo es almacenar un string en una variable denominada nombre, posteriormente ejecutar con un print, con el objetivo de mostrar en pantalla
+un mensaje de texto "Hola Juan, espero estes bien"
+"""
+nombre = input("Ingresa tu nombre: ")
+print(f'Hola {nombre}, espero estes bien.')
