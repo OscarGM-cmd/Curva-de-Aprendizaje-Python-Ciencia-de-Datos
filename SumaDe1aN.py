a33d349d39e40763d56ba9b5c610ab3c99a907f0
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-09-01 06:18:00 -0600

    Se le agregaron comentarios de como funciona el codigo

--- SumaDe1aN.py
+++ SumaDe1aN.py
@@ -1,7 +1,10 @@
 """
 Escribir un programa que lea un entero positivo, n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n.
 """
+
+# Este bloque funciona dado que se convierte en entero el limite, antes de convertirlo a entero se le pide un str con input, posteriormente
+# una variable llamada suma hace el calculo aritmetico para el resultado por ultimo usamos la funcion print para imprimir el resultado
+
 limite = int(input("Ingresa un numero entero: "))
 suma = limite*(limite+1)/2
 print(f'Tu resultado de la suma de 1 a {limite} es: {suma}')
-
