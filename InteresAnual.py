d327da44c3c0ddadee260fe4995243b92a34a1d3
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-09-01 14:14:40 -0600

    Se le agrego la explicacion de la funcionalidad del bloque de codigo

--- InteresAnual.py
+++ InteresAnual.py
@@ -1,7 +1,9 @@
 """
 Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión.
 """
-
+# En este bloque de codigo se declara tanto una variable flotante como una variable años e interes, estas son input,
+# posteriormente se calcula el capital en una variable con la misma denominacion usando su formula 
+# se muestra como ultimo en un output el resultado de la inversion
 monto = float(input("Ingresa la cantidad a invertir (Enteros): "))
 anios = float(input("Ingresa la cantidad de años (Años y): "))
 interes = float(input("Ingresa el interes anual (Porcentaje %): "))
