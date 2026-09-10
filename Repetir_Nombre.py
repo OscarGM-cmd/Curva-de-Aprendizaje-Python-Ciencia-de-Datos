757cc641981edb1c37a7165fc89c21d1408d63b7
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-09-10 17:39:09 -0600

    Evaluar la edad del frontend para inclusion de un modelo crediticio

--- Repetir_Nombre.py
+++ Repetir_Nombre.py
@@ -1,6 +1,7 @@
 """
 Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
 """
+# Se usan dos variables input, una es entera y la otra string, el objetivo es que muestre 3 veces el nombre en distintos niveles
 usuario = input("Ingresa un nombre: ")
 numero = int(input("Ingresa el numero de veces que quieres que se repita: "))
-print(f'{(usuario+ "\n")* 3}')
+print(f'{(usuario + "\n")* 3}')
