ca1e0b27a752ac1337f1aa541795fd040447ca84
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-09-16 00:49:03 -0600

    operadores aritmeticos

---
+++ Operadores/Operadores_Aritmeticos.py
@@ -0,0 +1,19 @@
+#Sumas y restas (+ y -)
+suma = 12 + 3
+resta = 12 -3
+
+#Multiplicacion y division (* y /)
+multiplicacion = 12 * 5
+division = 12 / 5
+
+#Potenciacion (Exponente) (**)
+exponente = 12**5
+
+#Resto o modulo
+resto = 12 % 5
+
+#Division baja
+division_baja = 12 // 5
+
+ 
+print(suma)
