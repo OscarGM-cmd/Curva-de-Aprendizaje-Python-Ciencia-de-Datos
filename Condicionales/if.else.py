42c81d8bc22be48233b10a16c9e2d9796d4a0d48
Oscar Galvan Moncada <galvannoscar@gmail.com>
2026-09-16 01:07:08 -0600

    Se hicieron operadores condicionales y aparte se uso if, else, elif

---
+++ Condicionales/if.else.py
@@ -0,0 +1,8 @@
+#Se prueba un condicional para entender la estructura y reglas de este condicional
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18: 
+    print("Puedes pasar")
+else:
+    print("No puedes pasar")
+print("Muchas Gracias por intentar")
---
+++ Operadores/Operadores_Compartaivos.py
@@ -0,0 +1,27 @@
+#Comparador de igualdad
+es_igual_que = 5 == 5
+
+#Comparador de desigualdad
+es_distinto_que = 5 != 5
+
+#Comparador de mayor 
+es_mayor_que = 5 > 5
+
+#Comparador de menor 
+es_mayor_que = 5 < 5
+
+#Comparador de mayor o igual
+es_mayor_que = 5 >= 5
+
+#Comparador de menor o igual
+es_mayor_que = 5 <= 5
+
+a = 5
+
+b = 10
+
+c = 20
+
+comparacion =  a + b < c
+
+print(comparacion)
